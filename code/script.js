9cb9b3e670c88f6c3bbb6cdc1f670d09e412d921
Vrutesh Mayekar <vruteshpm02@gmail.com>
2023-09-09 17:50:47 +0530

    added media query

--- code/script.js
+++ code/script.js
@@ -1,37 +1,40 @@
-let hrs=document.getElementById("hrs")
-let minutes=document.getElementById("min")
-let seconds=document.getElementById("sec")
+// time-section starts here
+
+let hrs = document.getElementById("hrs")
+let minutes = document.getElementById("min")
+let seconds = document.getElementById("sec")
 
 
 
-setInterval(()=>{
+setInterval(() => {
     let currentTime = new Date()
-    
-    hrs.innerHTML=(currentTime.getHours()<10?"0":"") + currentTime.getHours()
-    minutes.innerHTML=(currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes()
-    seconds.innerHTML=(currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds()
-},1000)
 
-let day=document.getElementById("day")
-let date=document.getElementById("date")
-let month=document.getElementById("month")
-let year=document.getElementById("year")
+    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours()
+    minutes.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes()
+    seconds.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds()
+}, 1000)
+// time-section ends here
 
-let daysofweek =['Sunday','Monday','Tuesday','Wednesday','Thrusday','Friday','Saturday'];
-let monthsofyear = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+// date-section starts here
+
+let day = document.getElementById("day")
+let date = document.getElementById("date")
+let month = document.getElementById("month")
+let year = document.getElementById("year")
+
+let daysofweek = ['Sunday,', 'Monday,', 'Tuesday,', 'Wednesday,', 'Thrusday,', 'Friday,', 'Saturday,'];
+let monthsofyear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 let currentDate = new Date()
 
-day.innerHTML=daysofweek[currentDate.getUTCDay()]
-date.innerHTML=(currentDate.getDate()<10?"0":"") + currentDate.getDate()
-month.innerHTML=monthsofyear[currentDate.getMonth()]
-year.innerHTML=currentDate.getFullYear()
+day.innerHTML = daysofweek[currentDate.getUTCDay()]
+date.innerHTML = (currentDate.getDate() < 10 ? "0" : "") + currentDate.getDate()
+month.innerHTML = monthsofyear[currentDate.getMonth()]
+year.innerHTML = currentDate.getFullYear()
 
+// date-section ends here
 
-
-
-
-// days.innerHTML=dateobj.getDay()
 
 
 
